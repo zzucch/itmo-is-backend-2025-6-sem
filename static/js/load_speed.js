494644610054c03762d027c7dd1795cbe4543e16
zzucch <nikitamilovatskiy@gmail.com>
2025-04-26 02:27:22 +0300

    feat: add request timing on frontend

--- static/js/load_speed.js
+++ static/js/load_speed.js
@@ -13,4 +13,21 @@ const startTime = performance.now();
 
     document.body.appendChild(loadTimeInfo);
   };
+
+  fetch(window.location.href, { method: "HEAD" })
+    .then((res) => {
+      const serverElapsed = res.headers.get("X-Elapsed-Time");
+
+      if (serverElapsed) {
+        const serverTimeInfo = document.createElement("div");
+        serverTimeInfo.innerText = `backend elapsed time: ${serverElapsed}`;
+        serverTimeInfo.style.backgroundColor = "#222";
+        serverTimeInfo.style.color = "#aaa";
+        serverTimeInfo.style.padding = "10px";
+        document.body.appendChild(serverTimeInfo);
+      }
+    })
+    .catch((err) => {
+      console.error("Failed to fetch server header:", err);
+    });
 })();
